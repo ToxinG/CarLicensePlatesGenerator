1c4d4fa7f14090d1d1cd77f45df2a98ced63852a
anton <toxan.m@gmail.com>
2021-01-21 22:22:11 +0300

    restored actual latest version

--- perspective_transformation.py
+++ perspective_transformation.py
@@ -3,6 +3,13 @@ import numpy as np
 import os
 
 
+def intersect(line1, line2):
+    det = line1[0] * line2[1] + line2[0] * line1[1]
+    x = (line2[1] * line1[2] - line1[1] * line2[2]) / det
+    y = (line2[0] * line1[2] - line1[0] * line2[2]) / det
+    return np.array([int(abs(x)), int(abs(y))])
+
+
 def perspective_transformation(mask, new_plate):
 
     old_h = 85  # 115 mm
@@ -12,7 +19,8 @@ def perspective_transformation(mask, new_plate):
 
     gray = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
     canny_output = cv.Canny(gray, 50, 150, apertureSize=3)
-    _, contours, _ = cv.findContours(canny_output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    contours, _ = cv.findContours(canny_output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     hull_list = []
     for i in range(len(contours)):
         hull = cv.convexHull(contours[i])
@@ -28,7 +36,11 @@ def perspective_transformation(mask, new_plate):
 
     black = cv.cvtColor(black, cv.COLOR_BGR2GRAY)
     canny_output = cv.Canny(black, 50, 150, apertureSize=3)
-    corners = cv.goodFeaturesToTrack(canny_output, 4, 0.01, 10)
+
+    # cv.imshow('canny', canny_output)
+    # cv.waitKey(0)
+
+    corners = cv.goodFeaturesToTrack(black, 4, 0.01, 10)
     corners = [c[0] for c in corners]
     corners.sort(key=lambda x: x[0])
     left = sorted(corners[:2], key=lambda x: x[1])
@@ -41,21 +53,59 @@ def perspective_transformation(mask, new_plate):
     M = cv.getPerspectiveTransform(np.array(corners_old, dtype='float32'), np.array(corners, dtype='float32'))
     warped = cv.warpPerspective(new_plate, M, (mask.shape[1], mask.shape[0]), borderMode=cv.BORDER_TRANSPARENT)
 
-    # cv.imshow('result', warped)
-    # cv.waitKey(0)
+    for c in corners:
+        print(c)
+        cv.circle(black, tuple(c), 4, 0, -1)
+
+    contours, _ = cv.findContours(black, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    edge_lines = []  # [a, b, c] (Ax + By = C)
+    for cnt in contours:
+        rows, cols = black.shape[:2]
+        [vx, vy, x1, y1] = cv.fitLine(cnt, cv.DIST_L2, 0, 0.01, 0.01).flatten()
+        print(vx, vy, x1, y1)
+        x2 = x1 + vx * 100
+        y2 = y1 + vy * 100
+        a = y2 - y1
+        b = x1 - x2
+        edge_lines.append([a, b, -(b * y1 + a * x1)])
+
+        if abs(vx) > 0.6:
+            lefty = int((-x1 * vy / vx) + y1)
+            righty = int(((cols - x1) * vy / vx) + y1)
+            cv.line(black, (0, lefty), (cols, righty), 100, 2)
+        else:
+            topx = int((-y1 * vx / vy) + x1)
+            bottomx = int(((rows - y1) * vx / vy) + x1)
+            cv.line(black, (topx, 0), (bottomx, rows), 100, 2)
+        # print(f'vx: {vx}, vy: {vy}, x: {x}, y: {y}')
+        # lefty = int((-x * vy / vx) + y)
+        # righty = int(((cols - x) * vy / vx) + y)
+        # print(lefty, righty)
+
+        cv.line(black, (int(x1), int(y1)), (int(x2), int(y2)), 100, 1)
+
+    edge_lines.sort(key=lambda x: abs(x[0]))
+    corners_true = []
+    corners_true.append(intersect(edge_lines[0], edge_lines[2]))
+    corners_true.append(intersect(edge_lines[0], edge_lines[3]))
+    corners_true.append(intersect(edge_lines[1], edge_lines[2]))
+    corners_true.append(intersect(edge_lines[1], edge_lines[3]))
+
+    for c in corners_true:
+        print(c)
+        print(c.shape, type(c))
+        cv.circle(black, tuple(c), 3, 255, -1)
+
+
+    cv.imshow('drawing', black)
+    cv.waitKey(0)
 
     return warped, corners
 
-    # for c in corners:
-    #     print(c)
-    #     cv.circle(black, tuple(c), 3, 255, -1)
-    # cv.imshow('corners', black)
-    # cv.waitKey(0)
-
 
 if __name__ == '__main__':
-    img_dir = 'images/plates_text'
-    for img_name in os.listdir(img_dir):
+    img_dir = 'images/masks'
+    for img_name in sorted(os.listdir(img_dir)):
         print(img_name)
         mask = cv.imread(os.path.join(img_dir, img_name))
         new_plate = cv.imread('images/new_plates/car_6895.jpg')
